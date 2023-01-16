9274093f32058debbde94488a55909ac5086da36
IJM <ioctnt18@gmail.com>
2023-01-16 22:39:56 +0900

    깃허브 작동 확인

--- app/src/main/java/com/android/tripapp/MainActivity.kt
+++ app/src/main/java/com/android/tripapp/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // 깃허브 작동 확인! - 지민
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
