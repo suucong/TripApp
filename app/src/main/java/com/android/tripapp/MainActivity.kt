8d2583a240d966216a489dabbf302ba063fde0ae
서가은 <98108245+Seo-Ga@users.noreply.github.com>
2023-01-16 23:22:19 +0900

    깃허브 작동 확인 - 가은

--- app/src/main/java/com/android/tripapp/MainActivity.kt
+++ app/src/main/java/com/android/tripapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     // 깃허브 작동 확인! - 지민
+    // 깃허브 작동 확인!! - 가은
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
