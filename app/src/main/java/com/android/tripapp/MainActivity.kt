0c9514d1f8720350b41ff2f76a8e540f0163ddfa
Keilly <keilly0104@naver.com>
2023-01-17 15:45:10 +0900

    로그인 화면 구현중

--- app/src/main/java/com/android/tripapp/MainActivity.kt
+++ app/src/main/java/com/android/tripapp/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.android.tripapp
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    // 깃허브 작동 확인! - 지민
-    // 깃허브 작동 확인!! - 가은
+    lateinit var edtEmail : EditText
+    lateinit var edtPassword : EditText
+    lateinit var btnLogin : Button
+    lateinit var btnSignin : Button
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtEmail = findViewById(R.id.edtEmail)
+        edtPassword = findViewById(R.id.edtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignin = findViewById(R.id.btnSignin)
+
+        btnLogin.setClickable(false)
+
+        if (edtEmail.text != null && edtPassword.text != null) {
+            btnLogin.setClickable(true)
+            //btnLogin.setBackgroundColor(Color.parseColor("#FF6700F9"))
+        }
+
+
+
+
     }
 }
