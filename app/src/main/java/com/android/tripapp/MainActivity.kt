1cf526cf5535438e0e1f15fa180f74a7efdcfe7a
Keilly <keilly0104@naver.com>
2023-01-17 16:32:15 +0900

    로그인 화면 구현(비번 일정 길이 이하는 버튼 비활성화)

--- app/src/main/java/com/android/tripapp/MainActivity.kt
+++ app/src/main/java/com/android/tripapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 
@@ -24,14 +26,25 @@ class MainActivity : AppCompatActivity() {
         btnLogin = findViewById(R.id.btnLogin)
         btnSignin = findViewById(R.id.btnSignin)
 
-        btnLogin.setClickable(false)
+    //    btnLogin.isEnabled = false
 
-        if (edtEmail.text != null && edtPassword.text != null) {
-            btnLogin.setClickable(true)
-            //btnLogin.setBackgroundColor(Color.parseColor("#FF6700F9"))
-        }
+        edtPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                //텍스트를 입력 후
 
-
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //텍스트 입력 전
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //텍스트 입력 중
+                if(edtPassword.length() < 4) { // 패스워드의 길이가 4미만이면
+                    btnLogin.isEnabled = false // 버튼 비활성화
+                } else {
+                    btnLogin.isEnabled = true // 버튼 활성화
+                }
+            }
+        })
 
 
     }
