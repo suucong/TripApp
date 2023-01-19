0519c9bfdc78de1c0ba0a95b364a7c2252972781
Keilly <keilly0104@naver.com>
2023-01-19 15:34:35 +0900

    기연_회원가입 시 toast 나오도록 설정

--- app/src/main/java/com/android/tripapp/MainActivity.kt
+++ app/src/main/java/com/android/tripapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.android.tripapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
