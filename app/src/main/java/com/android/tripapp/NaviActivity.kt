80e62fc12ab993f1dffd3ec0f418308fc01226dd
Keilly <keilly0104@naver.com>
2023-01-21 00:15:12 +0900

    기연_bottomNavigationView 화면 문제 해결

--- app/src/main/java/com/android/tripapp/NaviActivity.kt
+++ app/src/main/java/com/android/tripapp/NaviActivity.kt
@@ -2,56 +2,37 @@ package com.android.tripapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import com.android.tripapp.databinding.ActivityMainBinding
 import com.android.tripapp.databinding.ActivityNaviBinding
-import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.navigation.NavigationBarView
 
 class NaviActivity : AppCompatActivity() {
-
-    private lateinit var homeFragment: Fragment
-    private lateinit var planFragment: Fragment
-    private lateinit var weatherFragment: Fragment
-    private lateinit var tripLogFragment: Fragment
-    private lateinit var myPageFragment: Fragment
+    private lateinit var binding : ActivityNaviBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navi)
+        binding = ActivityNaviBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        homeFragment = HomeFragment()
-        planFragment = PlanFragment()
-        weatherFragment = WeatherFragment()
-        tripLogFragment = TripLogFragment()
-        myPageFragment = MyPageFragment()
+        changeFragment(HomeFragment())
 
-        supportFragmentManager.beginTransaction().replace(R.id.navigationView, planFragment).commit()
-
-        val bottomnavigationView = findViewById(R.id.navigationView) as NavigationBarView
-        bottomnavigationView.setOnItemSelectedListener (
-            object: NavigationBarView.OnItemSelectedListener {
-                override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                    var selectedFragment: Fragment ?= null
-                    when(item.itemId) {
-                        R.id.HomeFragment -> selectedFragment = homeFragment
-                        R.id.PlanFragment -> selectedFragment = planFragment
-                        R.id.WeatherFragment -> selectedFragment = weatherFragment
-                        R.id.TripLogFragment -> selectedFragment = tripLogFragment
-                        R.id.MyPageFragment -> selectedFragment = myPageFragment
-                    }
-                    selectedFragment?.let {
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.navigationView, selectedFragment)
-                            .commit()
-                        return true
-                    }
-                    return false
-                }
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.HomeFragment -> changeFragment(HomeFragment())
+                R.id.PlanFragment -> changeFragment(PlanFragment())
+                R.id.WeatherFragment -> changeFragment(WeatherFragment())
+                R.id.TripLogFragment -> changeFragment(TripLogFragment())
+                R.id.MyPageFragment -> changeFragment(MyPageFragment())
             }
-        )
+            true
+        }
     }
+
+    private fun changeFragment(fragment : Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainFrameLayout, fragment)
+            .commit()
+    }
+
 }
