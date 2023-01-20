9938040545cb3309532b16d6097cff19155e093d
Keilly <keilly0104@naver.com>
2023-01-20 21:53:32 +0900

    기연_bottomNavigationView 화면 전환 가능, xml 파일에 요소가 올라가긴 하지만 위치가 이상함. 수정 필요

--- app/src/main/java/com/android/tripapp/NaviActivity.kt
+++ app/src/main/java/com/android/tripapp/NaviActivity.kt
@@ -2,92 +2,56 @@ package com.android.tripapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.android.tripapp.databinding.ActivityNaviBinding
 import androidx.fragment.app.FragmentTransaction
-
-private const val TAG_HOME = "home_fragment"
-private const val TAG_PLAN = "plan_fragment"
-private const val TAG_WEATHER = "weather_fragment"
-private const val TAG_MY_PLAN = "my_plan_fragment"
-private const val TAG_MY_PAGE = "my_page_fragment"
+import com.google.android.material.navigation.NavigationBarView
 
 class NaviActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityNaviBinding
+    private lateinit var homeFragment: Fragment
+    private lateinit var planFragment: Fragment
+    private lateinit var weatherFragment: Fragment
+    private lateinit var tripLogFragment: Fragment
+    private lateinit var myPageFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNaviBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_navi)
 
-        binding.navigationView.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.HomeFragment -> setFragment(TAG_HOME, HomeFragment())
-                R.id.PlanFragment -> setFragment(TAG_PLAN, PlanFragment())
-                R.id.WeatherFragment -> setFragment(TAG_WEATHER, WeatherFragment())
-                R.id.MyPlanFragment -> setFragment(TAG_MY_PLAN, MyPlanFragment())
-                R.id.MyPageFragment -> setFragment(TAG_MY_PAGE, MyPageFragment())
-            }
-            true
-        }
-    }
-    private fun setFragment(tag: String, fragment: Fragment) {
-        val manager: FragmentManager = supportFragmentManager
-        val fragTransaction = manager.beginTransaction()
+        homeFragment = HomeFragment()
+        planFragment = PlanFragment()
+        weatherFragment = WeatherFragment()
+        tripLogFragment = TripLogFragment()
+        myPageFragment = MyPageFragment()
 
-        if (manager.findFragmentByTag(tag) == null){
-            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
-        }
+        supportFragmentManager.beginTransaction().replace(R.id.navigationView, planFragment).commit()
 
-        val home = manager.findFragmentByTag(TAG_HOME)
-        val plan = manager.findFragmentByTag(TAG_PLAN)
-        val weather= manager.findFragmentByTag(TAG_WEATHER)
-        val myPlan = manager.findFragmentByTag(TAG_MY_PLAN)
-        val myPage = manager.findFragmentByTag(TAG_MY_PAGE)
-
-        if (home != null) {
-            fragTransaction.hide(home)
-        }
-        if (plan != null) {
-            fragTransaction.hide(plan)
-        }
-        if (weather != null) {
-            fragTransaction.hide(weather)
-        }
-        if (myPlan != null) {
-            fragTransaction.hide(myPlan)
-        }
-        if (myPage != null) {
-            fragTransaction.hide(myPage)
-        }
-
-        if(tag == TAG_HOME) {
-            if(home!=null) {
-                fragTransaction.show(home)
+        val bottomnavigationView = findViewById(R.id.navigationView) as NavigationBarView
+        bottomnavigationView.setOnItemSelectedListener (
+            object: NavigationBarView.OnItemSelectedListener {
+                override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                    var selectedFragment: Fragment ?= null
+                    when(item.itemId) {
+                        R.id.HomeFragment -> selectedFragment = homeFragment
+                        R.id.PlanFragment -> selectedFragment = planFragment
+                        R.id.WeatherFragment -> selectedFragment = weatherFragment
+                        R.id.TripLogFragment -> selectedFragment = tripLogFragment
+                        R.id.MyPageFragment -> selectedFragment = myPageFragment
+                    }
+                    selectedFragment?.let {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.navigationView, selectedFragment)
+                            .commit()
+                        return true
+                    }
+                    return false
+                }
             }
-        }
-        else if(tag == TAG_PLAN) {
-            if(plan!=null) {
-                fragTransaction.show(plan)
-            }
-        }
-        else if(tag == TAG_WEATHER) {
-            if(weather!=null) {
-                fragTransaction.show(weather)
-            }
-        }
-        else if(tag == TAG_MY_PLAN) {
-            if(myPlan!=null) {
-                fragTransaction.show(myPlan)
-            }
-        }
-        else if(tag == TAG_MY_PAGE) {
-            if(myPage!=null) {
-                fragTransaction.show(myPage)
-            }
-        }
-        fragTransaction.commitAllowingStateLoss();
+        )
     }
 }
