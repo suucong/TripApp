1d27dcc3f03a7bacd67559ec976375b146b3f7bd
suucong <sophia021210@naver.com>
2023-01-21 10:20:57 +0900

    ..

--- app/src/main/java/com/android/tripapp/HomeFragment.kt
+++ app/src/main/java/com/android/tripapp/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import com.android.tripapp.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +18,18 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+//public class Frag1 extends Fragment
+//{
+//    priave
+//}
+
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,4 +66,4 @@ class HomeFragment : Fragment() {
                 }
             }
     }
-}
+    }
--- app/src/main/java/com/android/tripapp/NaviActivity.kt
+++ app/src/main/java/com/android/tripapp/NaviActivity.kt
@@ -2,6 +2,9 @@ package com.android.tripapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.android.tripapp.databinding.ActivityNaviBinding
@@ -89,5 +92,15 @@ class NaviActivity : AppCompatActivity() {
             }
         }
         fragTransaction.commitAllowingStateLoss();
+    }
+}
+
+class OneFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
