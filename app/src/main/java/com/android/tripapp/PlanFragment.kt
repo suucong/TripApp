4a99c9ecae81e24f6b903b57468800d94e870508
Keilly <keilly0104@naver.com>
2023-01-24 16:04:56 +0900

    기연_PlanFragment에 계획 기획하는 페이지 추가중

--- app/src/main/java/com/android/tripapp/PlanFragment.kt
+++ app/src/main/java/com/android/tripapp/PlanFragment.kt
@@ -1,12 +1,14 @@
 package com.android.tripapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -17,6 +19,7 @@ private const val ARG_PARAM2 = "param2"
 class PlanFragment : Fragment() {
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
+    lateinit var btnAddPlan : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +35,7 @@ class PlanFragment : Fragment() {
         val view:View = inflater.inflate(R.layout.fragment_plan, container, false)
         viewPager = view.findViewById(R.id.planViewPager)
         tabLayout = view.findViewById(R.id.planTab)
+        btnAddPlan = view.findViewById(R.id.btnAddPlan_Pn)
         return view
     }
 
@@ -60,5 +64,13 @@ class PlanFragment : Fragment() {
                 1 -> tab.text = "지난 계획"
             }
         }.attach()
+
+        // AddPlan button click event
+        btnAddPlan.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, PlanWhereActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/android/tripapp/PlanWhereActivity.kt
+++ app/src/main/java/com/android/tripapp/PlanWhereActivity.kt
@@ -1,11 +1,34 @@
 package com.android.tripapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PlanWhereActivity : AppCompatActivity() {
+
+    lateinit var btnBackPlan : Button
+    lateinit var btnNext_where : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan_where)
+
+        btnBackPlan = findViewById(R.id.btnBackPlan_Pnwr)
+        btnNext_where = findViewById(R.id.btnNext_Pnwr)
+
+        btnNext_where.setOnClickListener {
+            var intent = Intent(this, PlanWhenActivity::class.java)
+            startActivity(intent)
+        }
+
+//        btnBackPlan.setOnClickListener{
+//            var intent = Intent(this, NaviActivity::class.java)
+//            startActivity(intent)
+//            supportFragmentManager
+//                .beginTransaction()
+//                .replace(R.id.bottom_navigation_view, PlanFragment())
+//                .commit()
+//        }
     }
 }
